331af95c09d0d6aa7a5ae057cb1c3f060c967c94
Jeremy Thomas <bbxdesign@gmail.com>
2018-01-31 01:20:42 +0000

    Fix ff

--- javascript/collection.js
+++ javascript/collection.js
@@ -1,4 +1,38 @@
 document.addEventListener('DOMContentLoaded', function() {
+  var $root = document.documentElement;
+  var $alo = document.getElementById('alo');
+  var $bsaShadow = document.getElementById('bsaShadow');
+
+  var toTravel = $root.scrollHeight - window.innerHeight;
+  var fromTop = $alo.offsetTop;
+  var menuThrottle = null;
+
+  if ($alo) {
+    document.addEventListener('scroll', function(event) {
+      var scrollTop = window.scrollY;
+      setBsa(scrollTop);
+      clearTimeout(menuThrottle);
+      throttle = setTimeout(setBsaShadows(scrollTop), 100);
+    });
+
+    function setBsaShadows(scrollTop) {
+      var distance = toTravel - scrollTop;
+      var topFactor = 1 - (distance / toTravel);
+      $bsaShadow.style.opacity = topFactor;
+      $bsaShadow.style.transform = 'scaleY(' + topFactor + ')';
+    }
+
+    function setBsa(scrollTop) {
+      if (scrollTop >= fromTop) {
+        $alo.classList.add('is-fixed');
+      } else {
+        $alo.classList.remove('is-fixed');
+      }
+    }
+
+    setBsaShadows(0);
+  }
+
   // Hashes
   var $hashes = document.querySelectorAll('.hash, .menu-list-ul li a');
 
--- javascript/single.js
+++ javascript/single.js
@@ -1,2 +1,23 @@
 document.addEventListener('DOMContentLoaded', function() {
+  var $root = document.documentElement;
+  var toTravel = $root.scrollHeight - window.innerHeight;
+  var $bsaShadow = document.getElementById('bsaShadow');
+  var menuThrottle = null;
+
+  if ($bsaShadow) {
+    document.addEventListener('scroll', function(event) {
+      clearTimeout(menuThrottle);
+      throttle = setTimeout(setBsaShadows(), 100);
+    });
+
+    function setBsaShadows() {
+      var scrollTop = window.scrollY;
+      var distance = toTravel - scrollTop;
+      var topFactor = 1 - (distance / toTravel);
+      $bsaShadow.style.opacity = topFactor;
+      $bsaShadow.style.transform = 'scaleY(' + topFactor + ')';
+    }
+
+    setBsaShadows();
+  }
 });
